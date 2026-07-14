14659c7da845fa5e2f45c44da25d3d95288c7348
Ali-Hassan01 <alihassanmaqboolhusain@gmail.com>
2026-07-14 19:41:05 +0500

    added route files

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const helmet = require("helmet");
 const corsMiddleware = require("./config/cors");
+const rateLimiter = require("./config/rateLimiter");
 const logger = require("./config/logger");
 const routes = require("./routes");
 
@@ -14,6 +15,9 @@ app.use(corsMiddleware);
 
 // HTTP Request Logger
 app.use(logger);
+
+// rate limiter middlware
+app.use(rateLimiter);
 
 // Parse JSON Bodies
 app.use(express.json());
