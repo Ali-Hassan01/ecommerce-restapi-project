2b98f01cc124a8d44b31cb79751a8bc2baac93ad
Ali-Hassan01 <alihassanmaqboolhusain@gmail.com>
2026-07-15 16:33:24 +0500

    feat: add MongoDB database connection

--- src/config/env.js
+++ src/config/env.js
@@ -1,8 +1,7 @@
 require("dotenv").config();
 
-const env = {
-  port: process.env.PORT || 5000,
+module.exports = {
+  port: process.env.PORT || 3000,
   nodeEnv: process.env.NODE_ENV || "development",
+  mongoUri: process.env.MONGODB_URI,
 };
-
-module.exports = env;
--- src/server.js
+++ src/server.js
@@ -1,6 +1,13 @@
 const app = require("./app");
 const env = require("./config/env");
+const connectDatabase = require("./config/database");
 
-app.listen(env.port, () => {
-  console.log(`Server is running on http://localhost:${env.port}`);
-});
+const startServer = async () => {
+  await connectDatabase();
+
+  app.listen(env.port, () => {
+    console.log(`Server is running on http://localhost:${env.port}`);
+  });
+};
+
+startServer();
