3d2351f94ae8d7fc7f27f4a4c1ce14c657103d53
Ali-Hassan01 <alihassanmaqboolhusain@gmail.com>
2026-07-23 15:29:05 +0500

    feat: add role-based authorization middleware

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -34,6 +34,17 @@ const protect = asyncHandler(async (req, res, next) => {
   next();
 });
 
+const authorize = (...roles) => {
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      return next(new AppError("You are not authorized to perform this action.", 403));
+    }
+
+    next();
+  };
+};
+
 module.exports = {
   protect,
+  authorize,
 };
--- src/routes/v1/auth.routes.js
+++ src/routes/v1/auth.routes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const { register, login } = require("../../controllers/auth.controller");
-const { protect } = require("../../middlewares/auth.middleware");
+const { protect, authorize } = require("../../middlewares/auth.middleware");
 const router = express.Router();
 router.post("/register", register);
 router.post("/login", login);
@@ -8,6 +8,12 @@ router.get("/me", protect, (req, res) => {
   res.status(200).json({
     success: true,
     data: req.user.getPublicProfile(),
+  });
+});
+router.get("/admin", protect, authorize("admin"), (req, res) => {
+  res.status(200).json({
+    success: true,
+    message: "Welcome Admin!",
   });
 });
 module.exports = router;
